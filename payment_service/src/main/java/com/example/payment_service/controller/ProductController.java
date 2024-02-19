ad709eed657569134fd200283397049984d49549
kuk329 <kuk329@cau.ac.kr>
2024-02-19 16:36:47 +0900

    feat: Product dto 클래스 생성
    서비스간 Product 정보를 전달하기 위해 dto 클래스를 생성했습니다.
    
    (#18)

--- payment_service/src/main/java/com/example/payment_service/controller/ProductController.java
+++ payment_service/src/main/java/com/example/payment_service/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.payment_service.controller;
 
 import com.example.payment_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    /**
+     * 상품 목록을 조회하는 API
+     */
+    @GetMapping("")
+    public void getProductList(){
+
+    }
+
+
 
 
 
