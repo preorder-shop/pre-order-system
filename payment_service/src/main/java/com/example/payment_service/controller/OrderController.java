9f13190c5479ca6ff51f43937f3c8909c813e007
kuk329 <kuk329@cau.ac.kr>
2024-02-20 20:40:08 +0900

    feat: 내가 주문한 상품 조회 API
    특정 사용자가 주문한 상품 주문서 조회API 생성
    
    (#18)

--- payment_service/src/main/java/com/example/payment_service/controller/OrderController.java
+++ payment_service/src/main/java/com/example/payment_service/controller/OrderController.java
@@ -1,10 +1,14 @@
 package com.example.payment_service.controller;
 
+import com.example.payment_service.domain.order.dto.OrderResponseDto;
+import com.example.payment_service.service.OrderService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -13,11 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrderController {
 
+    private final OrderService orderService;
 
 
     /**
      * 주문정보 조회 API
      */
     @GetMapping("")
-    public ResponseEntity<>
+    public ResponseEntity<List<OrderResponseDto>> getMyOrder(@RequestParam(name = "userId") String userId){
+
+        List<OrderResponseDto> result = orderService.getMyOrderList(userId);
+
+        return ResponseEntity.ok().body(result);
+    }
 }
--- payment_service/src/main/java/com/example/payment_service/domain/order/dto/OrderResponseDto.java
+++ payment_service/src/main/java/com/example/payment_service/domain/order/dto/OrderResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.payment_service.domain.order.dto;
 
+import com.example.payment_service.domain.order.Order;
 import com.example.payment_service.domain.order.OrderState;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -8,8 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -20,4 +20,24 @@ public class OrderResponseDto {
     private String productNumber;
     private OrderState orderState;
     private LocalDateTime orderDate;
+
+    @Builder
+    private OrderResponseDto(Long id, String userId, String productNumber, OrderState orderState,LocalDateTime time){
+        this.id = id;
+        this.userId = userId;
+        this.productNumber = productNumber;
+        this.orderState = orderState;
+        this.orderDate = time;
+    }
+
+    static public OrderResponseDto of(Order order){
+        return OrderResponseDto.builder()
+                .id(order.getId())
+                .userId(order.getUserId())
+                .productNumber(order.getProductNumber())
+                .orderState(order.getOrderState())
+                .time(order.getUpdatedDateTime())
+                .build();
+
+    }
 }
