e8bc41cd995f5f80a14b9ee713aa8c1691a8c784
kuk329 <kuk329@cau.ac.kr>
2024-03-23 17:01:01 +0900

    chore: activity service 분리

--- src/main/java/com/example/reservation/ReservationApplication.java
+++ src/main/java/com/example/reservation/ReservationApplication.java
@@ -2,10 +2,8 @@ package com.example.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ReservationApplication {
 
     public static void main(String[] args) {
